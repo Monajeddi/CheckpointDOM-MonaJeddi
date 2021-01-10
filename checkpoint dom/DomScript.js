e5614812eae696ce3ba697416ffd2d675d2a824c
Monajeddi <76617488+Monajeddi@users.noreply.github.com>
2021-01-10 23:58:23 +0100

    Add files via upload

---
+++ checkpoint dom/DomScript.js
@@ -0,0 +1,22 @@
+
+// plus btn
+let plusBtn = document.getElementsByClassName("plusBtn")
+// let plusBtn = document.querySelectorAll(".plusBtn")
+// console.log(plusBtn[0])
+let qute = document.getElementsByClassName("qute")
+
+
+// for (let i = 0; i < plusBtn.length; i++) {
+plusBtn[0].addEventListener("click", function () {
+    qute[0].innerHTML++;
+    // qute[0].innerHTML = Number(qute[0].innerHTML) + 1;
+})
+// }
+
+// minusBtn
+//
+//let minusBtn = document.getElementsByClassName("minusBtn")
+//minusBtn[0].addEventListener("click", function () {
+   // qute[0].innerHTML--;
+
+//
